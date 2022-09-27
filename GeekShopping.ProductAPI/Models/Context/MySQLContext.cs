dbf57dfccb81afc6261f0f9351eea042c6b0325e
Talles Dias <tallesdelfino1@gmail.com>
2022-09-27 13:22:12 -0300

    populando banco de dados

--- GeekShopping.ProductAPI/Models/Context/MySQLContext.cs
+++ GeekShopping.ProductAPI/Models/Context/MySQLContext.cs
@@ -8,5 +8,67 @@ namespace GeekShopping.ProductAPI.Models.Context
         public MySQLContext(DbContextOptions<MySQLContext>options):base(options){}
         public DbSet<Product> Products { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Name",
+                Price = new decimal (69.9),
+                Description = "alguma coisa descrita aqui",
+                ImageUrl = "ainda nao tem",
+                CategoryName="camisa"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "alguma coisa descrita aqui",
+                ImageUrl = "ainda nao tem",
+                CategoryName = "camisa"
+            }); modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "alguma coisa descrita aqui",
+                ImageUrl = "ainda nao tem",
+                CategoryName = "camisa"
+            }); modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "alguma coisa descrita aqui",
+                ImageUrl = "ainda nao tem",
+                CategoryName = "camisa"
+            }); modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "alguma coisa descrita aqui",
+                ImageUrl = "ainda nao tem",
+                CategoryName = "camisa"
+            }); modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 7,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "alguma coisa descrita aqui",
+                ImageUrl = "ainda nao tem",
+                CategoryName = "camisa"
+            }); modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 8,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "alguma coisa descrita aqui",
+                ImageUrl = "ainda nao tem",
+                CategoryName = "camisa"
+            });
+        }
+
     }
 }
